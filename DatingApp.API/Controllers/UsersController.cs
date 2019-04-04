766c85be3624b2dbe3e18961d5d60303bfc9a79c
Jesse <jaymoe3141@gmail.com>
2019-04-04 00:05:14 -0700

    end of section 9

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -28,7 +28,7 @@ namespace DatingApp.API.Controllers
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
 
-            return Ok(new JsonResult(usersToReturn));
+            return Ok(usersToReturn);
         }
 
         [HttpGet("{id}")]
